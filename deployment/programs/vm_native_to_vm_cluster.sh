d09cb0f94c1b4d702e8c604e95eb196a306d9fb9
qinxiaowen <mail@qxw.im>
2024-08-06 17:27:59 +0800

    remove useless code

--- deployment/programs/vm_native_to_vm_cluster.sh
+++ deployment/programs/vm_native_to_vm_cluster.sh
@@ -3,19 +3,6 @@
 BASEDIR=$(dirname $0)
 . ${BASEDIR}/../scripts/lib_echo.sh
 
-# # create prometheus snapshot
-# echo_green_bold "Create vm-native data snapshot"
-# response=$(curl -s -X POST "localhost:${INTERNAL_VM_LISTEN_PORT}/snapshot/create")
-# if [ $? -ne 0 ]; then
-#     echo_red "request failed"
-#     exit 1
-# fi
-
-# echo "$response"
-# SNAPSHOT_NAME=$(echo "$response" | jq -r '.snapshot')
-# SNAPSHOT_PATH="${INTERNAL_VM_STORAGE_DIR}/snapshots/${SNAPSHOT_NAME}/"
-# echo -e "vm-native snapshot path: $SNAPSHOT_PATH\n"
-
 # import data to vm
 echo_green_bold "Importing vm-native to vm-cluster"
 START_TIME=$(date -d "-${REMOTE_VM_IMPORT_DAYS} days" -u +"%Y-%m-%dT%H:%M:%SZ")
